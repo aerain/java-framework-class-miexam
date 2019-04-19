5e15ba5860a3b6f7612cadde332ae97fde013f7c
aerain <j40f893@gmail.com>
2019-04-19 16:36:48 +0900

    8

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -19,7 +20,7 @@ public class UserDaoTests {
         userdao = applicationContext.getBean("userDao", UserDao.class);
     }
     @Test
-    public void testGet() throws SQLException, ClassNotFoundException {
+    public void testGet() throws SQLException{
         Long id = 1l;
         String name = "허윤호";
         String password = "1234";
@@ -29,7 +30,7 @@ public class UserDaoTests {
         assertThat(user.getPassword(), is(password));
     }
     @Test
-    public void testAdd() throws SQLException, ClassNotFoundException {
+    public void testAdd() throws SQLException {
         String name = "추가";
         String password = "1234";
         User user = new User();
@@ -43,6 +44,42 @@ public class UserDaoTests {
         assertThat(addUser.getId(), is(id));
         assertThat(addUser.getName(), is(name));
         assertThat(addUser.getPassword(), is(password));
+    }
+
+    @Test
+    public void testUpdate() throws SQLException {
+        String name = "추가업뎃";
+        String password = "123123";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        Long id = userdao.insert(user);
+
+        String willUpdateName = "바뀐업뎃";
+        String willUpdatePassword = "12313444";
+        User willUpdateUser = userdao.get(id);
+        willUpdateUser.setName(willUpdateName);
+        willUpdateUser.setPassword(willUpdatePassword);
+
+        userdao.update(willUpdateUser);
+
+        User updatedUser = userdao.get(id);
+        assertThat(updatedUser.getId(), is(id));
+        assertThat(updatedUser.getName(), is(willUpdateName));
+        assertThat(updatedUser.getPassword(), is(willUpdatePassword));
+    }
+
+    @Test
+    public void testDelete() throws SQLException {
+        String name = "줬따뺐기";
+        String password = "12345";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        Long id = userdao.insert(user);
+        userdao.delete(id);
+        User deletedUser = userdao.get(id);
+        assertThat(deletedUser, is(nullValue()));
     }
 //    @Test
 //    public void testHallaGet() throws SQLException, ClassNotFoundException {
